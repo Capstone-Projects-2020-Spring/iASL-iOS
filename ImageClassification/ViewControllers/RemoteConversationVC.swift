2157b5224c5101147b110d265cf5987323970caa
Ian Applebaum <Ian.tyler@electricbluefish.com>
2020-03-23 23:48:36 -0400

    Suggested coding style

--- ImageClassification/ViewControllers/RemoteConversationVC.swift
+++ ImageClassification/ViewControllers/RemoteConversationVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class RemoteConversationVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class RemoteConversationVC: UIViewController {
 
     var people = ["Ian Applebaum", "Leo Gomes", "Liam Miller", "Viet Pham", "Tarek Elseify", "Aidan Loza", "Shakeel Alibhai"]
 
@@ -18,7 +18,7 @@ class RemoteConversationVC: UIViewController, UITableViewDataSource, UITableView
     let backButton = UIButton()
     let tableView = UITableView()
     let liveButton = UIButton()
-    
+
     let logoutButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .clear
@@ -30,7 +30,16 @@ class RemoteConversationVC: UIViewController, UITableViewDataSource, UITableView
         button.addTarget(self, action: #selector(handleLogout), for: .touchUpInside)
         return button
     }()
-    
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+
+    //change the color of the status bar
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setNeedsStatusBarAppearanceUpdate()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +48,7 @@ class RemoteConversationVC: UIViewController, UITableViewDataSource, UITableView
         backButtonSetup()
         topLabelSetup()
         tableViewSetup()
-        
+
         logoutButtonSetup()
 
         //composedMessageSetup()
@@ -48,9 +57,9 @@ class RemoteConversationVC: UIViewController, UITableViewDataSource, UITableView
     //FIXME: Maybe add a pop up message asking the user if they are sure they want to log out?
     ///handles the logout button, so it logs the user out of firebae and presents the login controller
     @objc func handleLogout() {
-        
+
         print("handle logout tapped")
-        
+
         //log the user out of firebase
         do {
             try Auth.auth().signOut()
@@ -63,22 +72,61 @@ class RemoteConversationVC: UIViewController, UITableViewDataSource, UITableView
         loginController.modalPresentationStyle = .fullScreen
         present(loginController, animated: true, completion: nil)
     }
-    
+
     ///sets the anchors and adds the button to the top bar
     func logoutButtonSetup() {
         //x, y, height, width
         topBar.addSubview(logoutButton)
-        
+
         logoutButton.trailingAnchor.constraint(equalTo: topBar.trailingAnchor, constant: -10).isActive = true
         logoutButton.bottomAnchor.constraint(equalTo: topBar.bottomAnchor, constant: -10).isActive = true
         logoutButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
         //logoutButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
     }
-    
+
+	func topBarSetup() {
+		 view.addSubview(topBar)
+		 topBar.translatesAutoresizingMaskIntoConstraints = false
+		 topBar.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		 topBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+		 topBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+		 topBar.heightAnchor.constraint(equalToConstant: 90).isActive = true
+		 topBar.backgroundColor = .black
+	 }
+
+	 func backButtonSetup() {
+		 topBar.addSubview(backButton)
+		 backButton.translatesAutoresizingMaskIntoConstraints = false
+		 backButton.leadingAnchor.constraint(equalTo: topBar.leadingAnchor, constant: 20).isActive = true
+		 backButton.bottomAnchor.constraint(equalTo: topBar.bottomAnchor, constant: -10).isActive = true
+		 backButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+		 backButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+		 let tintedImage = #imageLiteral(resourceName: "back").withRenderingMode(.alwaysTemplate)
+		 backButton.setImage(tintedImage, for: .normal)
+		 backButton.tintColor = .white
+		 backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+	 }
+
+	 @objc func backButtonTapped() {
+		 dismiss(animated: true, completion: nil)
+		 //navigationController?.popViewController(animated: true)
+	 }
+
+	 func topLabelSetup() {
+		 topBar.addSubview(topLabel)
+		 topLabel.translatesAutoresizingMaskIntoConstraints = false
+		 topLabel.leadingAnchor.constraint(equalTo: backButton.trailingAnchor, constant: 20).isActive = true
+		 topLabel.bottomAnchor.constraint(equalTo: topBar.bottomAnchor, constant: -10).isActive = true
+		 topLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+		 topLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+		 topLabel.text = "Chat"
+		 topLabel.font = UIFont.boldSystemFont(ofSize: 30)
+		 topLabel.textColor = .white
+	 }
 
 }
 
-extension RemoteConversationVC {
+extension RemoteConversationVC: UITableViewDataSource, UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return people.count
@@ -91,25 +139,15 @@ extension RemoteConversationVC {
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+
         //let vc = ChatVC(collectionViewLayout: UICollectionViewFlowLayout())
-        
+
         let vc = ChatVC()
         vc.modalPresentationStyle = .fullScreen
         vc.modalTransitionStyle = .crossDissolve
         vc.topLabel.text = people[indexPath.row]
         present(vc, animated: true, completion: nil)
         //navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    //change the color of the status bar
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        setNeedsStatusBarAppearanceUpdate()
-    }
-    
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        .lightContent
     }
 
     func tableViewSetup() {
@@ -122,46 +160,6 @@ extension RemoteConversationVC {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.delegate = self
         tableView.dataSource = self
-    }
-
-    func topBarSetup() {
-        view.addSubview(topBar)
-        topBar.translatesAutoresizingMaskIntoConstraints = false
-        topBar.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        topBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        topBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        topBar.heightAnchor.constraint(equalToConstant: 90).isActive = true
-        topBar.backgroundColor = .black
-    }
-
-    func backButtonSetup() {
-        topBar.addSubview(backButton)
-        backButton.translatesAutoresizingMaskIntoConstraints = false
-        backButton.leadingAnchor.constraint(equalTo: topBar.leadingAnchor, constant: 20).isActive = true
-        backButton.bottomAnchor.constraint(equalTo: topBar.bottomAnchor, constant: -10).isActive = true
-        backButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        backButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
-        let tintedImage = #imageLiteral(resourceName: "back").withRenderingMode(.alwaysTemplate)
-        backButton.setImage(tintedImage, for: .normal)
-        backButton.tintColor = .white
-        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
-    }
-
-    @objc func backButtonTapped() {
-        dismiss(animated: true, completion: nil)
-        //navigationController?.popViewController(animated: true)
-    }
-
-    func topLabelSetup() {
-        topBar.addSubview(topLabel)
-        topLabel.translatesAutoresizingMaskIntoConstraints = false
-        topLabel.leadingAnchor.constraint(equalTo: backButton.trailingAnchor, constant: 20).isActive = true
-        topLabel.bottomAnchor.constraint(equalTo: topBar.bottomAnchor, constant: -10).isActive = true
-        topLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        topLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        topLabel.text = "Chat"
-        topLabel.font = UIFont.boldSystemFont(ofSize: 30)
-        topLabel.textColor = .white
     }
 
 }
