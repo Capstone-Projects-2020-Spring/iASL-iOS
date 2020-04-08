c0c7e46eb9261415f2cc780dd952ee9e832353f1
likhongomes <tug60499@temple.edu>
2020-04-08 14:11:18 -0400

    reverted back to login vc

--- ImageClassification/AppDelegate.swift
+++ ImageClassification/AppDelegate.swift
@@ -38,7 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         //FIXME: May need to reavaluate this solution
         //changes the root view controller
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ChatVC()
+        window?.rootViewController = LoginVC()
         window?.makeKeyAndVisible()
         
         //just for editing the chatVC
