9a867ec4a590aeafd4d6d3316a2c837bb6e3b015
likhongomes <tug60499@temple.edu>
2020-04-12 03:04:30 -0400

    working on word prediction filter logic

---
+++ ImageClassification/ViewControllers/PredictionAssist.swift
@@ -0,0 +1,23 @@
+//
+//  PredictionAssist.swift
+//  ImageClassification
+//
+//  Created by Likhon Gomes on 4/11/20.
+//  Copyright © 2020 Y Media Labs. All rights reserved.
+//
+
+import Foundation
+
+class PredictionAssit {
+    
+    init(){}
+    
+    func similarity(){
+        
+        let alphabets = [["A","S","E","M","N","T"],["G","H"],["I","J","I"],["K","P"],["U","V"]]
+            
+        
+        
+    }
+    
+}
