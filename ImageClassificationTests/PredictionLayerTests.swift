2ec7b8d5d3ed8156a9f17976660d0460cf133279
likhongomes <tug60499@temple.edu>
2020-04-18 14:00:07 -0400

    adding test cases for viewcontroller

--- ImageClassificationTests/PredictionLayerTests.swift
+++ ImageClassificationTests/PredictionLayerTests.swift
@@ -39,11 +39,11 @@ class PredictionLayerTests: XCTestCase {
     
     
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
