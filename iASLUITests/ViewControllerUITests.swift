cbcb437ad5ec50bee75086e968b0a1e09294530f
likhongomes <tug60499@temple.edu>
2020-04-21 01:54:21 -0400

    added some UI tests

--- iASLUITests/ViewControllerUITests.swift
+++ iASLUITests/ViewControllerUITests.swift
@@ -21,35 +21,59 @@ class ViewControllerUITests: XCTestCase {
     ///Checks the action to go to notes and then get back by pressing buttons
     func testTransitionToNotesAndBack(){
         
+        
+        let app = XCUIApplication()
+        app.buttons["notesIcon"].tap()
+        app.buttons["back"].tap()
+        
+                
     }
     
     ///Checks the action to go to contacts and then get back by pressing buttons
     func testTransitionToContactsAndBack(){
+        
+        let app = XCUIApplication()
+        app.buttons["chatIcon"].tap()
+        app.buttons["back"].tap()
         
     }
     
     ///Test output textview expansion and contraction by swiping up and down
     func testExpandAndContractOutputTextView(){
         
+        let textView = XCUIApplication().children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 2).children(matching: .textView).element
+        textView.swipeDown()
+        textView/*@START_MENU_TOKEN@*/.swipeUp()/*[[".swipeUp()",".swipeRight()"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
+        
     }
     
     ///Checks  the dashboard's expandability and contractability by tapping more button
     func testExpandAndContractDashboard(){
+        
+        let app = XCUIApplication()
+        app.buttons["More"].tap()
+        app.buttons["Close Dashboard"].tap()
         
     }
     
     ///Check the transition from main view controller to transcription vc by tapping on the live button
     func testTransitionToSpeechToTextAndBackByButtonTap(){
         
+        let app = XCUIApplication()
+        app.buttons["yo"].tap()
+        app.buttons["Stop Recording"].tap()
+        
     }
     
     ///Check the transition from main view controller to transcription vc by changing device orientation
     func testTransitionToSpeechToTextAndBackByButtonOrientationChange(){
+
         
     }
     
     ///Test by raising the keyboard and then type and then hit the keybaord button to remove the keybaord
     func testKeyboardExpandibility(){
+    
         
     }
     
@@ -61,30 +85,49 @@ class ViewControllerUITests: XCTestCase {
     ///Test slider by swiping it all ways and test the variable change
     func testSliderBySliding(){
         
+        let app = XCUIApplication()
+        let slider = app.sliders["50%"]
+        slider.tap()
+        slider.swipeRight()
+        
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element(boundBy: 2)
+        element.swipeLeft()
+        element.swipeRight()
+        app.sliders["0%"].swipeRight()
+        app.sliders["3%"].swipeRight()
+        
+ 
     }
 
     ///Test logout button by tapping it and logging out of the app
     func testLogOutButton(){
+        XCUIApplication().buttons["Log out"].tap()
         
     }
     
     ///test train button by tapping on it and moving on to Train View Controller
     func testTrainButton(){
-        
+        XCUIApplication().buttons["Help Us Learn ASL"].tap()
     }
     
     ///test prediction assist button by turning it on and off and observing it's state
     func testPredictionAssistButton(){
         
+        let app = XCUIApplication()
+        app.buttons["Prediction Assist: on"].tap()
+        app.buttons["Prediction Assist: off"].tap()
+        
     }
     
     ///Test clear button by removing something added to the output text view and then removing them
     func testClearButton(){
+        XCUIApplication().buttons["Clear"].tap()
         
     }
     
     ///Test by swiping right to check if the app transitions to chat view controller
     func testNavigationToChatBySwipingRight(){
+        
         
     }
     
