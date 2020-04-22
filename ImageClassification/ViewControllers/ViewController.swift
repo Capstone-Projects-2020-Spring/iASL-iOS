f32a9be910d126a88b20314872be7213fccc4f19
likhongomes <tug60499@temple.edu>
2020-04-21 20:02:54 -0400

    prediction on off working

--- ImageClassification/ViewControllers/ViewController.swift
+++ ImageClassification/ViewControllers/ViewController.swift
@@ -957,9 +957,14 @@ extension ViewController {
                 self.areaBound.isHidden = true
 				let confidence = self.result!.inferences[0].confidence
 				let prediction: String = self.result!.inferences[0].label.description
-                let swappedAlphabet = self.predictionLayer.letterProximitySwap(inputWord: self.outputTextView.text!, inputChar: prediction)
-                print("actual \(prediction) output \(swappedAlphabet)")
-                self.outputTextView.text.append(swappedAlphabet)
+                if !self.predictionAssistButton.isSelected {
+                    self.outputTextView.text.append(prediction)
+                } else {
+                    let swappedAlphabet = self.predictionLayer.letterProximitySwap(inputWord: self.outputTextView.text!, inputChar: prediction)
+                    print("actual \(prediction) output \(swappedAlphabet)")
+                    self.outputTextView.text.append(swappedAlphabet)
+                }
+                
 			}
 		}
 
